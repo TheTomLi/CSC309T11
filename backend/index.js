cd5d8289cde3757826276789c72e85d8b48eadad
Wenzhuoyi Li <wenzhuoyili@Wenzhuoyis-Laptop.local>
2025-04-06 15:38:19 -0400

    for deploy

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const routes = require("./routes");
+require('dotenv').config();
+const cors = require('cors');
+
+const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
+const app = express();
+app.use(express.json());
+app.use(cors({
+    origin: FRONTEND_URL,  // Use environment variable here
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+}));
+app.use('', routes);
+
+module.exports = app;
